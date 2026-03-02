39c8d618176dc319ce9244f0ff69617b224ce8f5
truittjanney <truittjanney93@gmail.com>
2026-03-02 12:14:23 -0600

    Created acm.tf, providers.tf, and s3.tf files

---
+++ acm.tf
@@ -0,0 +1,41 @@
+data "aws_route53_zone" "primary" {
+  name = "truittjanney.com"
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "portfolio_cert" {
+  provider = aws.us_east_1
+  domain_name = "truittjanney.com"
+  subject_alternative_names = ["www.truittjanney.com"]
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "Truitt Portfolio Certificate"
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.portfolio_cert.domain_validation_options : dvo.domain_name => {
+      name = dvo.resource_record_name
+      value = dvo.resource_record_value
+      type = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name = each.value.name
+  type = each.value.type
+  records = [each.value.value]
+  ttl = 60
+}
+
+resource "aws_acm_certificate_validation" "portfolio_cert_validation" {
+  provider = aws.us_east_1
+  certificate_arn = aws_acm_certificate.portfolio_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
---
+++ s3.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "portfolio_site" {
+    bucket = "truittjanney-terraform-site"
+
+    tags = {
+        Name = "Truitt Portfolio Site"
+        Environment = "production"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "portfolio_site" {
+    bucket = aws_s3_bucket.portfolio_site.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
