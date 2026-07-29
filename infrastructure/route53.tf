1fb94404c3e38825487eb00cbf1dd5ae7a048971
truittjanney <truittjanney93@gmail.com>
2026-07-29 18:04:55 -0500

    added Route 53 DNS setup for mediavault.truittjanney.com

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -1,35 +1,50 @@
 resource "aws_route53_record" "root" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name = "truittjanney.com."
-  type = "A"
+  name    = "truittjanney.com."
+  type    = "A"
 
   alias {
-    name = aws_cloudfront_distribution.prod.domain_name
-    zone_id = aws_cloudfront_distribution.prod.hosted_zone_id
+    name                   = aws_cloudfront_distribution.prod.domain_name
+    zone_id                = aws_cloudfront_distribution.prod.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name = "www.truittjanney.com."
-  type = "A"
+  name    = "www.truittjanney.com."
+  type    = "A"
 
   alias {
-    name = aws_cloudfront_distribution.prod.domain_name
-    zone_id = aws_cloudfront_distribution.prod.hosted_zone_id
+    name                   = aws_cloudfront_distribution.prod.domain_name
+    zone_id                = aws_cloudfront_distribution.prod.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
 resource "aws_route53_record" "dev" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name = "dev.truittjanney.com."
-  type = "A"
+  name    = "dev.truittjanney.com."
+  type    = "A"
 
   alias {
-    name = aws_cloudfront_distribution.dev.domain_name
-    zone_id = aws_cloudfront_distribution.dev.hosted_zone_id
+    name                   = aws_cloudfront_distribution.dev.domain_name
+    zone_id                = aws_cloudfront_distribution.dev.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+# Delegate MediaVault DNS management to its separate child hosted zone
+resource "aws_route53_record" "mediavault_delegation" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "mediavault.truittjanney.com."
+  type    = "NS"
+  ttl     = 300
+
+  records = [
+    "ns-460.awsdns-57.com.",
+    "ns-960.awsdns-56.net.",
+    "ns-2023.awsdns-60.co.uk.",
+    "ns-1156.awsdns-16.org.",
+  ]
 }
